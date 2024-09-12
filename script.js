f8baa952d02dad1b71a18777bddc304ec1251203
Robledo Pereira da Rosa <robledo.rosa@compasso.com.br>
2024-09-12 18:21:25 -0300

    Realizando mais alguns ajustes

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 window.onload = () => {
+
+    document.getElementById("searchButton").addEventListener("click", function() {
+        let searchTerm = document.getElementById("searchTerm").value.toLowerCase();
+        let sections = document.querySelectorAll("section");
+        let found = false;
+      
+        sections.forEach(function(section) {
+          let textContent = section.textContent.toLowerCase();
+          
+          if (textContent.includes(searchTerm)) {
+            section.scrollIntoView({ behavior: "smooth" }); // Rola até a seção encontrada
+            found = true;
+          }
+        });
+      
+        if (!found) {
+          alert("Termo não encontrado!");
+        }
+      });
+
+
     const buttonDown = document.getElementById("button-down");
     buttonDown.addEventListener("click", () => {
       window.scrollTo(0, 0);
     });
   };
+
+
+  
